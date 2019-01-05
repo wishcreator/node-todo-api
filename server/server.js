c7a1e15d965ece2d3b6b2dc39aee4c18fb941b79
Your Name <you@example.com>
2019-01-05 18:07:04 +0200

    Add delete todos by id

--- server/server.js
+++ server/server.js
@@ -47,6 +47,21 @@ app.get('/todos/:id', (req,res) => {
 
 });
 
+app.delete('/todos/:id', (req,res) => {
+  const {id} = req.params;
+  if(!ObjectID.isValid(id)) return res.status(404).send();
+
+  Todo.findByIdAndDelete(id)
+  .then(doc => {
+    if(!doc) return res.status(404).send();
+    res.status(200).send({doc});
+  }).catch(e => {
+    res.status(400).send();
+  });
+
+
+});
+
 
 app.listen(port,() => {
   console.log('Start at port '+ port);
