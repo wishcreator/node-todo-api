928d70748db0a80179519e7838b320739e4a83c8
Your Name <you@example.com>
2019-01-26 17:54:09 +0200

    setup seperate test and dev envs

---
+++ playground/mongose-delete.js
@@ -0,0 +1,11 @@
+const {ObjectID} = require('mongodb');
+
+const {mongose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/users');
+
+
+
+
+
+
